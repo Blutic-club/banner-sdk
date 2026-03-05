a00b1aa041873cfad90eeb939c0dadf494cec69e
Mishra Vaibhav <vaibhavmishra50@gmail.com>
2026-03-05 15:16:33 +0530

    minor change

--- src/CookieBanner.jsx
+++ src/CookieBanner.jsx
@@ -388,7 +388,7 @@ const CookieBanner = () => {
 
     // Track ignored interaction on page unload if no interaction occurred
     const handleBeforeUnload = () => {
-      if (!localStorage.getItem("browserId") && interactionId) {
+      if (!hasInteracted && interactionId) {
         trackIgnoredInteraction();
       }
     };
@@ -400,7 +400,7 @@ const CookieBanner = () => {
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
-  }, [interactionId]);
+  }, [interactionId, hasInteracted]);
 
   useEffect(() => {
     async function init() {
